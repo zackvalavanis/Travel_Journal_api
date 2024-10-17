1c44a644e322db9ec91a8c83693dbd1f0a9d3fc8
Zack <168221270+Zval321@users.noreply.github.com>
2024-10-16 20:40:26 -0500

    login

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,18 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  # Posts
   get 'posts' => 'posts#index'
   get 'posts/:id' => 'posts#show'
   patch 'posts/:id' => 'posts#update'
   post 'posts' => 'posts#create'
   delete 'posts/:id' => 'posts#destroy'
-
+  # Users
   post 'users' => 'users#create'
+  #Sessions 
+  post 'sessions' => 'sessions#create'
+
+
 
 
 
