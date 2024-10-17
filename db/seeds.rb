36ef4883fd4e0a035f27c20529db86c634bd6a1c
Zack <168221270+Zval321@users.noreply.github.com>
2024-10-16 20:58:14 -0500

    added first post, image and user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# user = User.new(
+#   name: 'Zack',
+#   email: 'zval321@gmail.com',
+#   password: 'password',
+#   password_confirmation: 'password'
+# )
+# user.save 
+
+
+
+
+# post = Post.new(
+#   title: 'My first post', 
+#   text: 'Hi all, and welcome to my travel blog. This is my first post on the website.',
+#   user_id: 1
+# )
+# post.save
+
+image = Image.new( 
+  post_id: 1, 
+  image_url: "https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bG9uZG9ufGVufDB8fDB8fHww"
+)
+image.save
